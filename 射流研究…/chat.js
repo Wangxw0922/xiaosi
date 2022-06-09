eacc926df48f2d2271f3adb437fad4d4db226ce7
Edison <56375900+Wangxw0922@users.noreply.github.com>
2022-06-09 11:06:08 +0800

    Update and rename js/chat.js to 射流研究…/chat.js

--- 射流研究…/chat.js
+++ 射流研究…/chat.js
@@ -22,7 +22,7 @@ $(function () {
   function getMsg(text) {
     $.ajax({
       method: 'GET',
-      url: 'http://www.liulongbin.top:3006/api/robot',
+      url: 'https://www.liulongbin.top:3006/api/robot',
       data: {
         spoken: text
       },
@@ -45,7 +45,7 @@ $(function () {
   function getVoice(text) {
     $.ajax({
       method: 'GET',
-      url: 'http://www.liulongbin.top:3006/api/synthesize',
+      url: 'https://www.liulongbin.top:3006/api/synthesize',
       data: {
         text: text
       },
